e6c9c1b98b0c5e564ec2b875c150ce836dad7ffe
Kiyoshi Stewart-Moore <61713205+Kiyostew@users.noreply.github.com>
2024-12-11 14:10:11 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,10 +13,6 @@
  *
  */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
 document.addEventListener("DOMContentLoaded", function () {
   const section1 = document.querySelector("#section1");
   const section2 = document.querySelector("#section2");
